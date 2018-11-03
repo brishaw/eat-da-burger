1e1b4bfad67e84cc097e28d8cd49e5342084a697
Brian <40767783+brishaw@users.noreply.github.com>
2018-11-03 04:51:25 -0400

    setting up for heroku push...

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- insert queries to populate the `burgers` table with at least three entries.
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Bloody Bisque', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Marinated Mayhem', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Crisco Coronary', false);
