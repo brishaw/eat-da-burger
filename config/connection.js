fa84db53074eafca473eb4a7b0f19aefa4519b1d
Brian <40767783+brishaw@users.noreply.github.com>
2018-11-05 18:37:30 -0500

    removed an old comment...

--- config/connection.js
+++ config/connection.js
@@ -15,24 +15,3 @@ if (process.env.JAWSDB_URL) {
 }
 connection.connect();
 module.exports = connection;
-
-
-// var mysql = require("mysql");
-
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "y+95D=E37uLNmr5}s",
-//   database: "burgers_db"
-// });
-
-// connection.connect(function (err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-// module.exports = connection;
